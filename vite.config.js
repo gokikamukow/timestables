b5ab4d5a8aecf29b121d437905075eeeecb3cc21
Paul Holmberg <pholm@dolby.com>
2024-02-25 13:59:45 +1100

    remove erroneous url call

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: url('pwa-192x192.png'),
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: url('pwa-512x512.png'),
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
