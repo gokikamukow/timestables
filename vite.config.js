11aa832915f8c30f3f5266e69667d449cf960cb9
Paul Holmberg <pholm@dolby.com>
2024-02-25 13:58:00 +1100

    Adde PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Timest Tables Practice',
+        short_name: 'TimesTables',
+        description: 'An app for practicing times tables',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: url('pwa-192x192.png'),
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: url('pwa-512x512.png'),
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   base: '/timestables',
 })
